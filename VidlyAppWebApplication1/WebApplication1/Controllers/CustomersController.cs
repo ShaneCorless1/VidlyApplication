46b7e655d085eb43b84cac384216cd5e48cddb87
Shane Corless <S00181114@mail.itsligo.ie>
2020-04-07 17:04:43 +0100

    Adding Entity Framework and displaying discounts and customers from database

--- VidlyAppWebApplication1/WebApplication1/Controllers/CustomersController.cs
+++ VidlyAppWebApplication1/WebApplication1/Controllers/CustomersController.cs
@@ -1,40 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class CustomersController : Controller
     {
         // GET: Customers
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var customers = GetCustomers();
 
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             return View(customer);
         }
 
-        public IEnumerable<Customer> GetCustomers()
+        public ActionResult New()
         {
-            return new List<Customer>
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
             {
-                new Customer {Id = 1, Name = "Snee"},
-                new Customer {Id = 2, Name = "Aaron Higgins"}
+                MembershipTypes = membershipTypes
+
             };
+            return View(viewModel);
+
         }
+
+        [HttpPost]
+        private ActionResult Create(NewCustomerViewModel viewModel)
+        {
+            return View();
+        }
+
+
     }
 }
