1a136aa6d039d51cb10d89fe2d5ed859f27d2586
Shane Corless <S00181114@mail.itsligo.ie>
2020-04-07 13:50:48 +0100

    Adding movie view and Links

--- VidlyAppWebApplication1/WebApplication1/Controllers/MoviesController.cs
+++ VidlyAppWebApplication1/WebApplication1/Controllers/MoviesController.cs
@@ -41,19 +41,20 @@ namespace WebApplication1.Controllers
 
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+            var movies = GetMovies();
 
-            if (String.IsNullOrWhiteSpace((sortBy)))
-            {
-                sortBy = "Name";
-            }
+            return View(movies);
+        }
 
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1:12)}")]
         public ActionResult ByReleaseDate(int year, int month)
